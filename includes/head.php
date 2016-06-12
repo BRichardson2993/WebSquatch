1531ae141d59fa311cf471492ae94718ce8c865b
Brian Richardson <Brian Richardson>
2016-06-11 23:48:50 -0500

    corrected viewport meta according to google's developer suggestion

--- includes/head.php
+++ includes/head.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name=viewport content="width=device-width, initial-scale=1">
 	<title>WebSquatch.com</title>
 	<link href='https://fonts.googleapis.com/css?family=Anton%7cFugaz+One%7cOpen+Sans+Condensed:300%7cMontserrat' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="styles.css">
